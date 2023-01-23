9d60f34fc59095cd9d6bbe8fcefe722d22f1d64f
Vlad Shkvarun <neshkvarun000@gmail.com>
2023-01-23 02:38:15 +0100

    init: finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+  let num = Math.abs(n);
+  let numToString = num.toString();
+  let numReverse = numToString.split('').reverse().join('');
+  return numReverse;
 }
